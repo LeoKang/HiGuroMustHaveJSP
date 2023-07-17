22b120130c83aec629882d13bada146f11649c27
Class01 <Class01@DESKTOP-9H6B9C5>
2023-07-17 15:56:58 +0900

    ~284 page. Write 기능 구현 완료

--- MustHaveJSP/src/main/java/model1/board/BoardDAO.java
+++ MustHaveJSP/src/main/java/model1/board/BoardDAO.java
@@ -66,4 +66,27 @@ public class BoardDAO extends JDBCConnect {
 
 		return bbs;
 	}
+
+	public int insertWrite(BoardDTO dto) {
+		int result = 0;
+
+		try {
+			String query = "INSERT INTO board ("
+					+ "num, title, content, id, visitcount) "
+					+ " VALUES ("
+					+ "seq_board_num.NEXTVAL, ?, ?, ?, 0)";
+
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, dto.getTitle());
+			psmt.setString(2, dto.getContent());
+			psmt.setString(3, dto.getId());
+
+			result = psmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("게시물 입력 중 예외 발생");
+			e.printStackTrace();
+		}
+
+		return result;
+	}
 }
